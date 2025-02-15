358a606cc7f974cca99445308a9e65a0e91c0843
Charles <charlesgiftangila@gmail.com>
2025-02-15 09:16:06 +0300

    Fix typo in Navbar: change "News % Insights" to "News & Insights" for improved clarity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,7 +101,7 @@ onClick={() => setIsMenuOpen((prev) => !prev)}
               className="font-montserrat font-medium text-[15px]  inline-flex items-center px-1 pt-1 text-gray-700 hover:text-orange-500 transition-colors focus:outline-none"
 onClick={() => setIsMenuOpen((prev) => !prev)}
             >
-             News % Insights
+             News & Insights
             </Link>
             <Link
               to="/contact"
