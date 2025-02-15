a081468f1a4f005946584b233c49ee82302901dd
Charles <charlesgiftangila@gmail.com>
2025-02-15 09:03:39 +0300

    Rename "Blogs" to "News & Insights" in Navbar for improved clarity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
               to="/blogs"
               className="font-montserrat font-medium text-[15px]  inline-flex items-center px-1 pt-1 text-gray-700 hover:text-orange-500 transition-colors focus:outline-none"
             >
-              Blogs
+             News & Insights
             </Link>
             
             
@@ -101,7 +101,7 @@ onClick={() => setIsMenuOpen((prev) => !prev)}
               className="font-montserrat font-medium text-[15px]  inline-flex items-center px-1 pt-1 text-gray-700 hover:text-orange-500 transition-colors focus:outline-none"
 onClick={() => setIsMenuOpen((prev) => !prev)}
             >
-              Blogs
+             News % Insights
             </Link>
             <Link
               to="/contact"
