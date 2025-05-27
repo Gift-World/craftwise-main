435457bd49dd2392c824d77ba1c961b45febe809
Charles Angila <charlesgiftangila@gmail.com>
2025-05-27 22:42:36 +0300

    Refactor code structure for improved readability and maintainability

--- src/components/RisingPro/HeroRising.jsx
+++ src/components/RisingPro/HeroRising.jsx
@@ -11,7 +11,7 @@ const HeroRising = () => {
       <div className="md:w-1/2 flex justify-center md:justify-start mb-6 md:mb-0">
         <div className="relative w-[300px] h-[300px] md:w-[400px] md:h-[400px]">
           <img 
-            src="/images/popup.avif" 
+            src="/images/popup.webp" 
             alt="Young professionals smiling" 
             className="w-full h-full object-cover rounded-full"
           />
